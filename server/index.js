d215605d4fcdbb0be7ea358ca55ce59bc9701079
akashbdj <akashbdj@gmail.com>
2020-10-22 14:54:17 +0530

    remove simulaton code

--- server/index.js
+++ server/index.js
@@ -6,58 +6,34 @@ const port = 3001
 
 app.use(cors())
 
-
-let timerId = null
-let c = 0
-
 app.get('/', (req, res) => {
   const cpus = os.cpus().length
   const loadAvgs = os.loadavg().map((avg) => (avg / cpus).toFixed(2))
 
-  console.log("current: ", c)
-  if (c === 7) {
-    res.send({
-      loads: [((Math.random() / 1.5) + 2), Math.random() / 2, Math.random() / 3],
-      time: new Date().getTime()
-    })
+  res.send({
+    loads: loadAvgs,
+    time: new Date().getTime()
+  })
 
-
-    if (!timerId) {
-      timerId = setInterval(() => {
-        c++
-        clearInterval(timerId)
-        timerId = null
-      }, 1.5 * 60 * 1000)
-    }
-
-    return
-  }
-
-  if (c === 2 || c == 18 || c == 70) {
-    res.send({
-      loads: [Math.random() / 2 + 1, Math.random() / 2, Math.random() / 3],
-      time: new Date().getTime()
-    })
-
-    if (!timerId) {
-      timerId = setInterval(() => {
-        c++
-        clearInterval(timerId)
-        timerId = null
-      }, 2.5 * 60 * 1000)
-    }
-  } else {
-    c++
-    res.send({
-      loads: loadAvgs,
-      time: new Date().getTime()
-    })
-  }
 })
-
-
-
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`)
 })
+
+
+// Used for simulating high load
+// let timerId = null
+// let c = 0
+// if (!timerId) {
+//   timerId = setInterval(() => {
+//     c++
+//     clearInterval(timerId)
+//     timerId = null
+//   }, 2 * 60 * 1000)
+// }
+// if (c === 7) {
+//   res.send({
+//     loads: [((Math.random() / 1.5) + 2), Math.random() / 2, Math.random() / 3],
+//     time: new Date().getTime()
+// })
